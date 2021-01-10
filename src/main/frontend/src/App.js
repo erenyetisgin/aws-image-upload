737fbda8e299f94537ea84cf9c32969929525ed0
Eren Yetisgin <erenyetiskin@hotmail.com>
2021-01-10 16:52:42 +0300

    UserProfiles component is now in a seperate file and returns data from backend

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,24 +1,25 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
+// import React, { useState, useEffect } from "react";
+import UserProfiles from "./components/UserProfiles";
+// import logo from "./logo.svg";
 import "./App.css";
-import axios from "axios";
+// import axios from "axios";
 
-const UserProfiles = () => {
-  const fetchUserProfiles = () => {
-    axios
-      .get("http://localhost:8080/api/v1/user-profile")
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => console.log(err));
-  };
+// const UserProfiles = () => {
+//   const fetchUserProfiles = () => {
+//     axios
+//       .get("http://localhost:8080/api/v1/user-profile")
+//       .then((res) => {
+//         console.log(res);
+//       })
+//       .catch((err) => console.log(err));
+//   };
 
-  useEffect(() => {
-    fetchUserProfiles();
-  }, []);
+//   useEffect(() => {
+//     fetchUserProfiles();
+//   }, []);
 
-  return <h1>Hello</h1>;
-};
+//   return <h1>Hello</h1>;
+// };
 
 function App() {
   return (
---
+++ src/main/frontend/src/components/UserProfiles.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function UserProfiles() {
+  const [userProfiles, setUserProfiles] = useState([]);
+
+  const fetchUserProfiles = () => {
+    axios
+      .get("http://localhost:8080/api/v1/user-profile")
+      .then((res) => {
+        console.log(res);
+        setUserProfiles(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchUserProfiles();
+  }, []);
+
+  return userProfiles.map((userProfile, index) => {
+    return (
+      <div key={index}>
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileId}</p>
+      </div>
+    );
+  });
+}
+
+export default UserProfiles;
