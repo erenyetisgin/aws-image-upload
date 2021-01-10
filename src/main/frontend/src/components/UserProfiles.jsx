ab4150bc42caf850c261c0921ebc2510c520e068
Eren Yetisgin <erenyetiskin@hotmail.com>
2021-01-10 17:53:16 +0300

    React drop zone is added

--- src/main/frontend/src/components/UserProfiles.jsx
+++ src/main/frontend/src/components/UserProfiles.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import DropZone from "./DropZone";
 
 function UserProfiles() {
   const [userProfiles, setUserProfiles] = useState([]);
@@ -21,6 +22,7 @@ function UserProfiles() {
   return userProfiles.map((userProfile, index) => {
     return (
       <div key={index}>
+        <DropZone />
         <h1>{userProfile.username}</h1>
         <p>{userProfile.userProfileId}</p>
       </div>
